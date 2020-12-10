df7be28de12e4ce84ea76c5916146f5db87221c7
csav4223 <david.arnold@student.uibk.ac.at>
2020-12-10 14:06:16 +0100

    changed passenger_id

--- src/main/java/GateChangeAlertClasses/GetGPS.java
+++ src/main/java/GateChangeAlertClasses/GetGPS.java
@@ -2,7 +2,12 @@ package GateChangeAlertClasses;
 
 public class GetGPS {
 
-    static public String getGPS(String passenger)
+    /**
+     * receives the passenger_id, gets it's gps location stored in the database and returns it
+     * @param passenger_id id of the passenger
+     * @return the gps location of the passenger according to the database
+     */
+    static public String getGPS(int passenger_id)
     {
         String gpsLocatoin = null;
         return gpsLocatoin;
