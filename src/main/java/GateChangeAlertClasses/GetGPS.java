71065cb41da990a8d56f298ed17e3e1a742f6e9d
csav4223 <david.arnold@student.uibk.ac.at>
2020-12-10 17:21:26 +0100

    access to our database and trying to get entries from there

--- src/main/java/GateChangeAlertClasses/GetGPS.java
+++ src/main/java/GateChangeAlertClasses/GetGPS.java
@@ -1,5 +1,7 @@
 package GateChangeAlertClasses;
 
+import java.sql.*;
+
 public class GetGPS {
 
     /**
@@ -7,10 +9,20 @@ public class GetGPS {
      * @param passenger_id id of the passenger
      * @return the gps location of the passenger according to the database
      */
-    static public String getGPS(int passenger_id)
+    public static String getGPS(int passenger_id) throws ClassNotFoundException, SQLException
     {
-        String gpsLocatoin = null;
-        return gpsLocatoin;
+        StringBuilder sqlCommand = new StringBuilder("select gps from PASSENGER where passenger_id = ");
+        sqlCommand.append(passenger_id);
+         
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://db-airport.cyw2qnj2xex2.us-east-1.rds.amazonaws.com"
+                      + "user=root&password=password");
+
+        PreparedStatement gpsLocatoin = con.prepareStatement(String.valueOf(sqlCommand));
+        ResultSet result = gpsLocatoin.executeQuery();
+
+        return result.getString("gps");
+  
     }
 
 }
---
+++ src/main/java/GateChangeAlertClasses/maindeletthis.java
@@ -0,0 +1,61 @@
+package GateChangeAlertClasses;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.rds.AmazonRDS;
+import com.amazonaws.services.rds.AmazonRDSClientBuilder;
+import com.amazonaws.services.rds.model.DBInstance;
+import com.amazonaws.services.rds.model.DescribeDBInstancesResult;
+
+
+public class maindeletthis {
+
+    public static void main(String[] args) {
+
+        //Authenticate RDS Client
+        AmazonRDS awsRDS = AmazonRDSClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
+
+        //Let's fetch all the databases created in AWS Console
+        DescribeDBInstancesResult dbInstResult = awsRDS.describeDBInstances();
+
+        List<DBInstance> dbInstances = dbInstResult.getDBInstances();
+
+        for (DBInstance dbInst : dbInstances) {
+            System.out.println("DB Instance:: " + dbInst.getDBName());
+        }
+
+        //Let's Connect to our database
+        String RDS_INSTANCE_HOSTNAME = "db-airport.cyw2qnj2xex2.us-east-1.rds.amazonaws.com";
+        String RDS_INSTANCE_PORT = "3306";
+
+        String JDBC_URL = "jdbc:oracle:thin:@" + RDS_INSTANCE_HOSTNAME + ":" + RDS_INSTANCE_PORT + ":db-airport";
+
+        try {
+
+            Connection connection = DriverManager.getConnection(
+                    JDBC_URL, "root", "password");
+
+            //verify the connection is successful
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("select gps from PASSENGER where passenger_id = 1");
+            while (rs.next()) {
+                String gps = rs.getString("gps");
+                System.out.println(gps);
+            }
+
+            //close the connection
+            stmt.close();
+            connection.close();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    }
