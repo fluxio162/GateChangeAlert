fdac367c9853c92b826cc9f08ac4197740e7fbe0
fluxio162 <48515164+fluxio162@users.noreply.github.com>
2020-12-11 12:56:25 +0100

    aws rekognition

--- src/main/java/GateChangeAlertClasses/CalcCheckDelay.java
+++ src/main/java/GateChangeAlertClasses/CalcCheckDelay.java
@@ -1,12 +1,65 @@
 package GateChangeAlertClasses;
 
+import com.amazonaws.services.rekognition.AmazonRekognition;
+import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
+import com.amazonaws.services.rekognition.model.*;
+
+import java.util.List;
+
 public class CalcCheckDelay {
 
     static public Double calcCheckDelay()
     {
         Double securityCheckDelay = null;
+
         
         return securityCheckDelay;
     }
 
+    private static void processImage(){
+        AmazonRekognition rekognitionClient = AmazonRekognitionClientBuilder.defaultClient();
+
+        String image = "security queue.jpg";
+
+        String s3Bucket ="wittibucket";
+
+        S3Object s3Obj = new S3Object();
+        s3Obj.withBucket(s3Bucket);
+        s3Obj.withName(image);
+
+        Image img = new Image();
+        img.withS3Object(s3Obj);
+
+        DetectLabelsRequest request = new DetectLabelsRequest();
+        request.withImage(img);
+        request.withMaxLabels(10);
+        request.withMinConfidence(90F);
+
+        int personCount = 0;
+
+        try {
+
+            DetectLabelsResult result = rekognitionClient.detectLabels(request);
+            List<Label> labels = result.getLabels();
+
+            for(Label label: labels)
+            {
+                System.out.println("Label ::" + label.getName());
+                System.out.println("Confidence ::" + label.getConfidence());
+
+                if(label.getName().equals("Person")){
+                    personCount = label.getInstances().size();
+
+                }
+            }
+        }
+        catch(AmazonRekognitionException e)
+        {
+            e.printStackTrace();
+        }
+
+        System.out.println("Total persons: "+personCount);
+    }
 }
+
+
--- src/main/java/GateChangeAlertLambdas/LambdaCalcCheckDelay.java
+++ src/main/java/GateChangeAlertLambdas/LambdaCalcCheckDelay.java
@@ -2,12 +2,60 @@ package GateChangeAlertLambdas;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.rekognition.AmazonRekognition;
+import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
+import com.amazonaws.services.rekognition.model.*;
 
+import java.util.List;
 import java.util.Map;
 
 public class LambdaCalcCheckDelay implements RequestHandler<Map<String, Object>, Map<String, Object>>{
         @Override public Map<String, Object> handleRequest(Map<String, Object> input, Context context){
             System.out.println("Hello World from CalcCheckDelay ");
         return null;
+    }
+    private static void processImage(){
+        AmazonRekognition rekognitionClient = AmazonRekognitionClientBuilder.defaultClient();
+
+        String image = "security queue.jpg";
+
+        String s3Bucket ="wittibucket";
+
+        S3Object s3Obj = new S3Object();
+        s3Obj.withBucket(s3Bucket);
+        s3Obj.withName(image);
+
+        Image img = new Image();
+        img.withS3Object(s3Obj);
+
+        DetectLabelsRequest request = new DetectLabelsRequest();
+        request.withImage(img);
+        request.withMaxLabels(10);
+        request.withMinConfidence(90F);
+
+        int personCount = 0;
+
+        try {
+
+            DetectLabelsResult result = rekognitionClient.detectLabels(request);
+            List<Label> labels = result.getLabels();
+
+            for(Label label: labels)
+            {
+                System.out.println("Label ::" + label.getName());
+                System.out.println("Confidence ::" + label.getConfidence());
+
+                if(label.getName().equals("Person")){
+                    personCount = label.getInstances().size();
+
+                }
+            }
+        }
+        catch(AmazonRekognitionException e)
+        {
+            e.printStackTrace();
+        }
+
+        System.out.println("Total persons: "+personCount);
     }
 }
