6f5939a9f555f2915666f10aa87869dba185ccff
chris <wittauch@outlook.de>
2020-12-17 13:50:05 +0100

    getPassengers

--- src/main/java/GateChangeAlertClasses/GetPassengers.java
+++ src/main/java/GateChangeAlertClasses/GetPassengers.java
@@ -4,18 +4,18 @@ import java.util.ArrayList;
 
 public class GetPassengers {
 
-    static public String getPassenger(int newGate)
+    static public ArrayList<String> getPassenger(int flight_id)
     {
         String passenger = null;
         int passenger_id=2;
         ArrayList<String> valueList = new ArrayList<String>();
 
-        valueList = DatabaseAccessClass.accessDatabase("Select * From PASSENGER Where PASSENGER.passenger_id ="+ passenger_id,"firstname");
-        return valueList.get(0);
+        valueList = DatabaseAccessClass.accessDatabase("SELECT passenger_id from PASSENGER WHERE flight_id = " + flight_id, "passenger_id");
+        return valueList;
     }
 
     public static void main(String[] args) {
-        System.out.println(getPassenger(4));
+        System.out.println(getPassenger(1));
     }
 
 
